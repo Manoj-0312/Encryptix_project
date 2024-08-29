cadfa1f44a7dcffe315f7683a6b4c83f1aeea150
Manoj-0312 <indian945200@gmail.com>
2024-08-29 23:57:57 +0530

    Update script.js file

--- Task - 1/src/scripts.js
+++ Task - 1/src/scripts.js
@@ -1,18 +1,26 @@
 const display = document.getElementById("display");
+num = [0,1,2,3,4,5,6,7,8,9]
+op = ["+","-","*","/"];
+open_brac = ["(","{","["];
+close_brac = [")","}","]"];
+isDotPresent = false;
+
 
 function appendToDisplay(input) {
     const lastChar = display.value.slice(-1);
 
-    if (/[\d]/.test(input) || input === '(') {
+
+    if (num.includes(input) || open_brac.includes(input)) {
         display.value += input;
     } 
-    else if (input === '.' && !/\.\d*$/.test(display.value)) {
+    else if (input === '.' && !isDotPresent) {
+        display.value += input;
+        isDotPresent = true;
+    } 
+    else if (op.includes(lastChar) && !op.includes(input)) {
         display.value += input;
     } 
-    else if (/[\+\-\*\/\%]/.test(input) && !/[\+\-\*\/\%\(\)]$/.test(lastChar)) {
-        display.value += input;
-    } 
-    else if (input === ')' && (display.value.match(/\(/g) || []).length > (display.value.match(/\)/g) || []).length) {
+    else if (close_brac.includes(input)) {
         display.value += input;
     }
 }
